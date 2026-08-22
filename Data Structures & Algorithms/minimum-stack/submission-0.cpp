24c616736fc246428432994e3ad781c757ab6f3c
Dipen <dipenbhandari205@outlook.com>
2026-08-22 09:41:54 -0500

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,27 @@
+class MinStack {
+public:
+    stack<int> s;
+    stack<int>minStack;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s.push(val);
+        val = min(val,minStack.empty() ? val:minStack.top());
+        minStack.push(val);
+    }
+    
+    void pop() {
+        s.pop();
+        minStack.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return minStack.top();   
+    }
+};
