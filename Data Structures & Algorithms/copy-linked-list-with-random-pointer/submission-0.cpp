53401f0a6432a6c8b390eb191396dd3dc520f815
Dipen <dipenbhandari205@outlook.com>
2026-08-30 12:18:45 -0500

    Add: copy-linked-list-with-random-pointer - submission-0

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-0.cpp
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return nullptr;
+        unordered_map<Node*,Node*>map;
+       for(Node* curr = head; curr != nullptr; curr=curr->next){
+        map[curr] = new Node(curr->val);
+       }
+        
+        for(Node* curr = head; curr!=nullptr; curr=curr->next){
+            map[curr]->next = map[curr->next];
+            map[curr]->random = map[curr->random]; 
+        }
+
+        return map[head];
+    }
+};
